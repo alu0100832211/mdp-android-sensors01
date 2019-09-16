e7d57832d6c03521bb6c33733104b0581917bc5b
Martín Belda <martin.belda.sosa@alumnos.upm.es>
2019-09-16 19:49:28 +0200

    Shows accelerometer values

--- app/src/main/java/dte/masteriot/mdp/sensors01/MainActivity.java
+++ app/src/main/java/dte/masteriot/mdp/sensors01/MainActivity.java
@@ -14,11 +14,11 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    Button bLight;
-    TextView tvSensorValue;
+    Button bLight, bAccelerometer;
+    TextView tvSensorValue, axValue, ayValue, azValue;
     private SensorManager sensorManager;
-    private Sensor lightSensor;
-    boolean lightSensorIsActive;
+    private Sensor lightSensor, accelerometerSensor;
+    boolean lightSensorIsActive, accelerometerSensorIsActive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +26,22 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
 
         lightSensorIsActive = false;
+        accelerometerSensorIsActive = false;
 
         // Get the references to the UI:
         bLight = findViewById(R.id.bLight); // button to start/stop sensor's readings
+        bAccelerometer = findViewById(R.id.bAccelerometer);
         tvSensorValue = findViewById(R.id.lightMeasurement); // sensor's values
+        axValue = findViewById(R.id.ax); // sensor's values
+        ayValue = findViewById(R.id.ay); // sensor's values
+        azValue = findViewById(R.id.az); // sensor's values
+
 
         // Get the reference to the sensor manager and the sensor:
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         // Obtain the reference to the default light sensor of the device:
         lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         // Listener for the button:
         bLight.setOnClickListener(new View.OnClickListener() {
@@ -57,6 +64,31 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 }
             }
         });
+        // Listener for the Accelerometer:
+        bAccelerometer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (accelerometerSensorIsActive) {
+                    // unregister listener and make the appropriate changes in the UI:
+                    sensorManager.unregisterListener(MainActivity.this, accelerometerSensor);
+                    bAccelerometer.setText(R.string.accelerometer_sensor_off);
+                    bAccelerometer.setBackground(getResources().getDrawable(R.drawable.round_button_off, getTheme()));
+                    axValue.setText("x = None");
+                    ayValue.setText("y = None");
+                    azValue.setText("z = None");
+                    accelerometerSensorIsActive = false;
+                } else {
+                    // register listener and make the appropriate changes in the UI:
+                    sensorManager.registerListener(MainActivity.this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+                    bAccelerometer.setText(R.string.accelerometer_sensor_on);
+                    bAccelerometer.setBackground(getResources().getDrawable(R.drawable.round_button_on, getTheme()));
+                    axValue.setText("x = Waiting");
+                    ayValue.setText("y = Waiting");
+                    azValue.setText("z = Waiting");
+                    accelerometerSensorIsActive = true;
+                }
+            }
+        });
     }
 
     // Methods related to the SensorEventListener interface:
@@ -64,7 +96,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         // Show the sensor's value in the UI:
-        tvSensorValue.setText(Float.toString(sensorEvent.values[0]));
+        if(sensorEvent.sensor.getType() == Sensor.TYPE_LIGHT) {
+            tvSensorValue.setText(Float.toString(sensorEvent.values[0]));
+        }
+        else if(sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+            axValue.setText(Float.toString(sensorEvent.values[0]));
+            ayValue.setText(Float.toString(sensorEvent.values[1]));
+            azValue.setText(Float.toString(sensorEvent.values[2]));
+        }
 
         Log.d("sensorEvent", "Blabla");
     }

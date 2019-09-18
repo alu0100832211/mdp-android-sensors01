2e387a42b93217db80de1537870811a2fb70819d
Martin Belda <alu0100832211@ull.edu.es>
2019-09-18 16:16:12 +0200

    3 sensors "working"

--- app/src/main/java/dte/masteriot/mdp/sensors01/MainActivity.java
+++ app/src/main/java/dte/masteriot/mdp/sensors01/MainActivity.java
@@ -13,45 +13,52 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.util.Log;
 
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    String TAG = "debug";
 
-    Button bLight, bAccelerometer;
-    TextView tvSensorValue, axValue, ayValue, azValue;
+    Button bLight, bAccelerometer, bStepCount;
+    TextView tvSensorValue, axValue, ayValue, azValue, stepCountValue;
+    private int stepCount;
     private SensorManager sensorManager;
-    private Sensor lightSensor, accelerometerSensor;
-    boolean lightSensorIsActive, accelerometerSensorIsActive;
+    private Sensor lightSensor, accelerometerSensor, stepSensor;
+    boolean lightSensorIsActive, accelerometerSensorIsActive, stepSensorIsActive;
 
-    @Override
-    public void onStop(){
-        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putBoolean("lightSensorIsActive", lightSensorIsActive);
-        editor.commit();
-    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: creating");
         setContentView(R.layout.activity_main);
 
         lightSensorIsActive = false;
         accelerometerSensorIsActive = false;
+        stepSensorIsActive = false;
+        stepCount = 0;
+        Log.d(TAG, "onCreate: accelerometer Sensor is inactive");
 
         // Get the references to the UI:
         bLight = findViewById(R.id.bLight); // button to start/stop sensor's readings
         bAccelerometer = findViewById(R.id.bAccelerometer);
+        bStepCount = findViewById(R.id.bStepCount);
         tvSensorValue = findViewById(R.id.lightMeasurement); // sensor's values
         axValue = findViewById(R.id.ax); // sensor's values
         ayValue = findViewById(R.id.ay); // sensor's values
         azValue = findViewById(R.id.az); // sensor's values
-
+        stepCountValue = findViewById(R.id.stepCountValue);
 
         // Get the reference to the sensor manager and the sensor:
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         // Obtain the reference to the default light sensor of the device:
         lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
 
+        this.setListeners();
+    }
+
+    public void setListeners(){
         // Listener for the button:
         bLight.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,7 +84,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         bAccelerometer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.d("debug", "Accelerometer clicked");
                 if (accelerometerSensorIsActive) {
+                    Log.d(TAG, "onClick: Light Sensor is Active");
                     // unregister listener and make the appropriate changes in the UI:
                     sensorManager.unregisterListener(MainActivity.this, accelerometerSensor);
                     bAccelerometer.setText(R.string.accelerometer_sensor_off);
@@ -87,14 +96,29 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     azValue.setText("z = None");
                     accelerometerSensorIsActive = false;
                 } else {
+                    Log.d(TAG, "onClick: lightsensorinactive");
                     // register listener and make the appropriate changes in the UI:
                     sensorManager.registerListener(MainActivity.this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
                     bAccelerometer.setText(R.string.accelerometer_sensor_on);
                     bAccelerometer.setBackground(getResources().getDrawable(R.drawable.round_button_on, getTheme()));
-                    axValue.setText("x = Waiting");
-                    ayValue.setText("y = Waiting");
-                    azValue.setText("z = Waiting");
                     accelerometerSensorIsActive = true;
+                }
+            }
+        });
+        bStepCount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(stepSensorIsActive){
+                    sensorManager.unregisterListener(MainActivity.this, stepSensor);
+                    bStepCount.setText("Steps\nON");
+                    bStepCount.setBackground(getResources().getDrawable(R.drawable.round_button_on, getTheme()));
+                    stepSensorIsActive = false;
+                }
+                else{
+                    sensorManager.registerListener(MainActivity.this, stepSensor, SensorManager.SENSOR_DELAY_NORMAL);
+                    bStepCount.setBackground(getResources().getDrawable(R.drawable.round_button_off, getTheme()));
+                    bStepCount.setText("Steps\nOFF");
+                    stepSensorIsActive = true;
                 }
             }
         });
@@ -113,8 +137,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             ayValue.setText(Float.toString(sensorEvent.values[1]));
             azValue.setText(Float.toString(sensorEvent.values[2]));
         }
-
-        Log.d("sensorEvent", "Blabla");
+        else if(sensorEvent.sensor.getType() == Sensor.TYPE_STEP_DETECTOR){
+            stepCount += sensorEvent.values[0];
+            Log.d(TAG, "onSensorChanged: SE HA DADO UN PASO!" + sensorEvent.values[0]);
+            stepCountValue.setText(Integer.toString(stepCount));
+        }
     }
 
     @Override
